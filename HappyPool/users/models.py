f0c04addfc80ccb723c5924ef5e0d649597e2e61
Pournami Pradeep <pournamipradeep13@gmail.com>
2025-05-13 10:03:12 +0530

    changed app name

---
+++ HappyPool/users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    password = models.CharField(max_length=8)
+    email = models.EmailField(unique=True)
+    profile_photo = models.FileField(upload_to="media/profiles",null=True,blank=True)
+    license_number = models.CharField(max_length=16)
